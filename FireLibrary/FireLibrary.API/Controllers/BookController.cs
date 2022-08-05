bb8bb8f7aa230192f14bafc95f3f7baf16c63107
alex <alejandro136@revature.net>
2022-08-05 15:01:34 -0700

    added book and a book reponse models where a book is a book and a response is another object which contains book | i just thought book should not cointain copies available?

--- FireLibrary/FireLibrary.API/Controllers/BookController.cs
+++ FireLibrary/FireLibrary.API/Controllers/BookController.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using FireLibrary.Model;
 namespace FireLibrary.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api")]
     public class BookController : ControllerBase
     {
         public BookController()
         {
         }
+        [HttpGet("books")]
+        public async Task<ActionResult<Book>> GetBookAsync(string title, string author, string isbn)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
 
---
+++ FireLibrary/FireLibrary.Model/BookResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace FireLibrary.Model
+{
+    public class BookResponse
+    {
+        Book Book { get; set; }
+        int TotalCopies { get; set; }
+        int CopiesAvailable { get; set; }
+        public BookResponse()
+        {
+        }
+        public BookResponse(Book book, int totalcopies, int copiesavailable)
+        {
+            this.Book = book;
+            this.TotalCopies = totalcopies;
+            this.TotalCopies = copiesavailable;
+        }
+    }
+}
+
