22d848ee85a067938b3633563fea5792093b58e3
alex <alejandro136@revature.net>
2022-08-05 12:57:44 -0700

    added the bare minimum of controller

---
+++ FireLibrary/FireLibrary.API/Controllers/CustomerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FireLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase 
+    {
+        public CustomerController()
+        {
+        }
+    }
+}
+
