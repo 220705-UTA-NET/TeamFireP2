73ebb4ac42d581efe97e0a2220efc4ac4af255b3
alex <alejandro136@revature.net>
2022-08-05 18:58:59 -0700

    wrote incomplete test for book controller get book method. book model not decided yet and get book method in BookController throws not implemented so cannot test for content value but I think its a good start

--- FireLibrary/FireLibrary.API/Controllers/BookController.cs
+++ FireLibrary/FireLibrary.API/Controllers/BookController.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using FireLibrary.Model;
+using FireLibrary.Data;
 namespace FireLibrary.API.Controllers
 {
     [ApiController]
     [Route("api")]
     public class BookController : ControllerBase
     {
-        public BookController()
+        private readonly IRepository _repo;
+        private readonly ILogger<BookController> _logger;
+
+        public BookController(IRepository repo, ILogger<BookController> logger)
         {
+            this._repo = repo;
+            this._logger = logger;
         }
         [HttpGet("books")]
         public async Task<ActionResult<Book>> GetBookAsync(string title, string author, string isbn)
         {
+            await Task.Delay(1000);
             throw new NotImplementedException();
         }
 
---
+++ FireLibrary/FireLibrary.Tests/BookControllerTest.cs
@@ -0,0 +1,31 @@
+﻿namespace FireLibrary.Tests;
+using Xunit;
+using Xunit.Sdk;
+using Moq;
+using Microsoft.Extensions.Logging;
+using FireLibrary.API.Controllers;
+using FireLibrary.Model;
+using FireLibrary.Data;
+using Microsoft.AspNetCore.Mvc;//needed to get type ContentResult
+
+public class BookControllerTest
+{
+    [Fact]
+    public void GetBook_Input_Result()
+    {
+        //given
+        //arrange
+        Book book = new Book();//whatever book will be//needed to make a proper book object with title and all that
+        Mock<ILogger<BookController>> mockLogger = new();
+        Mock<IRepository> mockRepo = new();
+        var bookController = new BookController(mockRepo.Object, mockLogger.Object);//added the mock logger to satisfy constructor
+        string json_should_be = @"{""author"":""author""}";
+        //act
+        //var result = await bookController.GetBookAsync("title", "author", "isbn");//once implemented should return a task and could use await
+        var result = bookController.GetBookAsync("title", "author", "isbn");
+        Assert.IsType<ContentResult>(result);//did it return action result
+        Assert.IsType<ActionResult>(result);//did it return an action result//action result is abstract class -> that means it returns content?
+        //Assert.Equal(json_should_be, result.Content);//it tells me content not defined in result | but I think it will return content
+        Assert.Equal(json_should_be, result.Value);//might not have value?//not sure how to test what returned content
+    }
+}
