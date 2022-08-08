7bef60584930731a3d74b296e9a9f59cca60ef23
Jonathan De La Cruz <jdelacruznode@gmail.com>
2022-08-08 18:25:10 -0400

    removed msrp

--- FireLibrary/FireLibrary.Model/Book.cs
+++ FireLibrary/FireLibrary.Model/Book.cs
@@ -7,7 +7,6 @@
         public string? Publisher { get; set; }
         public string? Language { get; set; }
         public int Pages { get; set; }
-        public double Msrp { get; set; }
         public int AuthorId { get; set; }
         public string? Synopsys { get; set; }
         public string? Excerpt { get; set; }
