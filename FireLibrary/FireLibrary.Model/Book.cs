c1bc9f1d957e5996f858495efa2a883f49dbc36b
jdelacruz96 <92655139+jdelacruz96@users.noreply.github.com>
2022-08-08 18:27:38 -0400

    removed msrp from Book.cs

--- FireLibrary/FireLibrary.Model/Book.cs
+++ FireLibrary/FireLibrary.Model/Book.cs
@@ -7,7 +7,6 @@
         public string? Publisher { get; set; }
         public string? Language { get; set; }
         public int Pages { get; set; }
-        public double Msrp { get; set; }
         public int AuthorId { get; set; }
         public string? Synopsys { get; set; }
         public string? Excerpt { get; set; }
@@ -17,14 +16,13 @@
 
         public Book() { }
 
-        public Book(string? isbn, string? title, string? publisher, string? language, int pages, double msrp, int authorId, string? synopsys, string? excerpt, int totalCopies, int avalableCopies)
+        public Book(string? isbn, string? title, string? publisher, string? language, int pages, int authorId, string? synopsys, string? excerpt, int totalCopies, int avalableCopies)
         {
             Isbn = isbn;
             Title = title;
             Publisher = publisher;
             Language = language;
             Pages = pages;
-            Msrp = msrp;
             AuthorId = authorId;
             Synopsys = synopsys;
             Excerpt = excerpt;
