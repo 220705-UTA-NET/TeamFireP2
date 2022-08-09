3a686922771cbdc21b3f3bf784f2d7d5e8f6d4bd
alex <alejandro136@revature.net>
2022-08-09 14:51:32 -0700

    added 404 not found to controller for giggles | added return hard coded books and authors so we can deploy a working api

--- FireLibrary/FireLibrary.API/Controllers/CustomerController.cs
+++ FireLibrary/FireLibrary.API/Controllers/CustomerController.cs
@@ -18,11 +18,15 @@ namespace FireLibrary.API.Controllers
             this._logger = logger;
         }
         [HttpGet("customer")]
-        public async Task<ActionResult<Customer>> GetCustomerAsync(int? id,string? username)
+        public async Task<ActionResult<Customer>> GetCustomerAsync(int? id, string? username)
         {
             //return one fake customer hard coded for now
-            Customer customer = new Customer(12345, "username1", true, 0, 0);//this should pass test by cheating
+            Customer customer = new Customer(12345, "username1", true, 1, 1);//this should pass test by cheating
             //Customer customer = await _repo.GetCustomerAsync(12345);//um-implemented maybe get customer by id and get customer by username could be two separate methods
+            if (customer.Username == "" || id == -1)//customer is null added the -1 to check for a user which does not exists while we wait for LibraryRepo
+            {
+                return NotFound();
+            }
             string json = JsonSerializer.Serialize(customer);
             var result = new ContentResult()
             {
