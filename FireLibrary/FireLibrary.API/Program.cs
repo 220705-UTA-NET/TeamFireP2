8c874fb1d1812a388e1eed98077036e59cc269c4
Jonathan De La Cruz <jdelacruznode@gmail.com>
2022-08-09 11:30:30 -0400

    split SQLRepository into Auth and Library repos

--- FireLibrary/FireLibrary.API/Program.cs
+++ FireLibrary/FireLibrary.API/Program.cs
@@ -1,11 +1,33 @@
+using FireLibrary.Data;
+using Microsoft.Extensions.Logging;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+//string connectionStringLibrary = builder.Configuration.GetConnectionString("connectionStringLibrary");
+//string connectionStringAuth = builder.Configuration.GetConnectionString("connectionStringAuth");
+
+
+//Adding CORS, allowing any origin
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*");
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IRepository>(spLibrary => new LibraryRepository(connectionStringLibrary, spLibrary.GetRequiredService<ILogger<LibraryRepository>>()));
+builder.Services.AddSingleton<IRepository>(spAuth => new AuthRepository(connectionStringAuth, spAuth.GetRequiredService<ILogger<LibraryRepository>>()));
 
 var app = builder.Build();
 
@@ -16,7 +38,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//Displaying swagger on build
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
