cb3f76f2a2ad74c95b8dc8d230adfab21a805b66
Abanoub Sadek <abanoubfetaha@Abanoubs-MacBook-Pro.local>
2022-08-08 18:56:43 -0700

    fixed the test

---
+++ FireLibrary/FireLibrary.Model/CustomerResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace FireLibrary.Model
+{
+    public class CustomerResponse
+    {
+
+        public Customer Customer { get; set; }
+        public int OutstandingBooks { get; set; }
+        public int Fines { get; set; }
+
+        public CustomerResponse() { }
+    
+        public CustomerResponse(Customer customer, int outstandingbooks, int fines)
+        {
+            this.Customer = customer;
+            this.OutstandingBooks = outstandingbooks;
+            this.Fines = fines;
+        }
+    }
+}
+
--- FireLibrary/FireLibrary.Tests/CustomerControllerTest.cs
+++ FireLibrary/FireLibrary.Tests/CustomerControllerTest.cs
@@ -17,10 +17,20 @@ namespace FireLibrary.Tests
         public void GetCustomer_Input_Result()
         {
             Customer customer = new Customer(12345, "username1", true, 0);
-            System.Console.WriteLine("help me");
+            CustomerResponse customerResponse = new CustomerResponse();
+            customerResponse.Customer = customer;
+            customerResponse.OutstandingBooks = 0;
+            customerResponse.Fines = 0;
+            System.Console.WriteLine("customer test ");
             Mock<ILogger<CustomerController>> mockLogger = new();
             Mock<IRepository> mockRepo = new();
             var customerController = new CustomerController(mockRepo.Object, mockLogger.Object);
+
+            string json_should_be = JsonSerializer.Serialize(customerResponse);
+            System.Console.WriteLine(json_should_be);
+            json_should_be = @"{""Customer"":{""CustomerID"":12345,""Username"":""username1"",""CanBorrow"":true,""BookCount"":0},""OutstandingBooks"":0,""Fines"":0}";
+            System.Console.WriteLine(json_should_be);
+
             var result = customerController.GetCustomerAsync(12345, "username1");
 
             Assert.IsType<ContentResult>(result);//did it return action result
@@ -28,4 +38,3 @@ namespace FireLibrary.Tests
         }
     }
 }
-
