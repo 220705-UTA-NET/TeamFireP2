ca4f08271fe3acd3960d1ceaaaf212e3f3991854
Abanoub Sadek <abanoubfetaha@Abanoubs-MacBook-Pro.local>
2022-08-08 17:21:31 -0700

    abanoub add

---
+++ FireLibrary/FireLibrary.Tests/CustomerControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using Xunit.Sdk;
+using Moq;
+using Microsoft.Extensions.Logging;
+using FireLibrary.API.Controllers;
+using FireLibrary.Model;
+using FireLibrary.Data;
+using Microsoft.AspNetCore.Mvc;//needed to get type ContentResult
+using System.Text.Json;
+
+namespace FireLibrary.Tests
+{
+
+    public class CustomerControllerTest
+    {
+        [Fact]
+        public void GetCustomer_Input_Result()
+        {
+            Customer customer = new Customer(12345, "username1", true, 0);
+            System.Console.WriteLine("help me");
+            Mock<ILogger<CustomerController>> mockLogger = new();
+            Mock<IRepository> mockRepo = new();
+            var customerController = new CustomerController(mockRepo.Object, mockLogger.Object);
+            var result = customerController.GetCustomerAsync(12345, "username1");
+
+            Assert.IsType<ContentResult>(result);//did it return action result
+            Assert.IsType<ActionResult>(result);
+        }
+    }
+}
+
