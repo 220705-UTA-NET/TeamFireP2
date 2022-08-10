cc12f6f5c8ea14824eb892052612b80c4100c8f6
Hau Nguyen <hau263@revature.com>
2022-08-09 20:53:42 -0400

    Rebasing with dev  Please enter the commit message for your changes. Lines starting

---
+++ FireLibrary/FireLibrary.Data/SQLRepository.cs
@@ -0,0 +1,63 @@
+﻿using FireLibrary.Model;
+using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+
+namespace FireLibrary.Data
+{
+    public class SQLRepository : IRepository
+    {
+        
+
+
+        // Fields
+        private readonly string _connectionString;
+        private readonly ILogger<SQLRepository> _logger;
+        public SQLRepository(string connectionString, ILogger<SQLRepository> logger)
+        {
+            _connectionString = connectionString;
+            _logger = logger;
+        }
+
+
+        //Methods
+        /*
+         * Methods to address.
+        Task<Book> GetBookIsbnAsync();
+        Task<IEnumerable<Book>> GetBooksAsync();
+        Task<IEnumerable<Book>> GetBooksByGenre();
+        Task<Customer> GetCustomerAsync();
+        */
+
+        public async Task<IEnumerable<Book>> GetBooksAsync() //getting a list of all books
+        {
+            List<Book> booklist = new();
+
+            using SqlConnection connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync();
+
+            // Could also use "SELECT * FROM FireLibrary.Books" or w/e fields we wantm but make sure that we add the fields
+            string cmdText = "SELECT ISBN, Title, Pages FROM FireLibrary.Books";
+            using SqlCommand cmd = new(cmdText, connection);
+            using SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                int isbn = reader.GetInt32(0);
+                string title = reader.GetString(1);
+                int pages = reader.GetInt32(2);
+
+                Book tmpBook = new Book(isbn, title, pages); //gotta make this shit in Model
+                booklist.Add(tmpBook);
+            }
+
+            await connection.CloseAsync();
+            _logger.LogInformation()("Executed GetBooksAsync, returned {0} results", booklist.Count); 
+        }
+
+
+
+
+
+
+    }
+}
--- FireLibrary/FireLibrary.Model/Book.cs
+++ FireLibrary/FireLibrary.Model/Book.cs
@@ -7,17 +7,20 @@
         public string? Publisher { get; set; }
         public string? Language { get; set; }
         public int Pages { get; set; }
+        public string? Genre { get; set; }
         public int AuthorId { get; set; }
         public string? AuthorName { get; set; }
         public string? Synopsys { get; set; }
         public string? Excerpt { get; set; }
         public int TotalCopies { get; set; } = 5;
         public int AvalableCopies { get; set; } = 5;
+        
 
 
         public Book() { }
 
         public Book(string? isbn, string? title, string? publisher, string? language, int pages, int authorId, string? authorname, string? synopsys, string? excerpt, int totalCopies, int avalableCopies)
+
         {
             Isbn = isbn;
             Title = title;
